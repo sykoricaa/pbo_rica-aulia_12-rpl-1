d88af2982a8ed77adfec1263f509b0f775869b96
sykoricaa <rikagstnlitz10@gmail.com>
2025-09-15 10:45:28 +0700

    nt.js

---
+++ nt.js
@@ -0,0 +1,9 @@
+function storyMalam() {
+  document.body.style.backgroundColor = "#121212"; 
+  document.getElementById("output").innerHTML = `
+    <h2 style="color:#9c0c0c;">Malam Bersama Mas Sankara</h2>
+    <p>
+      Saat malam tiba, waktuku bersama Mas Sankara. <b>${nama}</b> dan Mas Sankara duduk di teras rumah sambil menikmati angin malam. Ia mendengarkan ceritaku tentang hari yang sudah kulalui, lalu memberikan nasihat dengan lembut. Terkadang kami menonton <b>${nontonApa}</b> bersama atau sekadar berbincang sambil menatap langit malam. Bersamanya, malam terasa hangat dan menenangkan, menutup hari dengan penuh rasa <b>${rasaApa}</b>.
+    </p>
+  `;
+}
