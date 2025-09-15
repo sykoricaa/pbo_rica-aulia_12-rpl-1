f5567770ea7fc7f86e39b5b3aca1fcec271ebdf1
sykoricaa <rikagstnlitz10@gmail.com>
2025-09-15 10:43:24 +0700

    mor.js

---
+++ mor.js
@@ -0,0 +1,9 @@
+function storyPagi() {
+  document.body.style.backgroundColor = "#1a1a1a";
+  document.getElementById("output").innerHTML = `
+    <h2 style="color:#b71c1c;">Pagi Bersama Mas Sylus</h2>
+    <p>
+<b>${nama}</b> terbangun di pagi hari dengan udara yang masih segar. Mas Sylus sudah lebih dulu bangun dan menyiapkan <b>${sarapan}</b> sederhana. Kami duduk berdua di meja makan, berbincang ringan tentang rencana hari ini sambil <b>${ngapain}</b>. Pagi terasa damai karena bisa memulai hari bersama dengan senyum dan perhatian darinya.
+    </p>
+  `;
+}
