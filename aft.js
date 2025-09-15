464786bb96daedf5f4b78b0897fa50e3a16b5365
sykoricaa <rikagstnlitz10@gmail.com>
2025-09-15 10:44:23 +0700

    aft.js

---
+++ aft.js
@@ -0,0 +1,9 @@
+function storySiang() {
+  document.body.style.backgroundColor = "#2c2c2c";
+  document.getElementById("output").innerHTML = `
+    <h2 style="color:#d32f2f;">Siang Bersama Mas Shi Yan</h2>
+    <p>
+      Menjelang siang, <b>${nama}</b> bertemu Mas Shi Yan. Kami menghabiskan waktu dengan berjalan santai dan bercerita <b>${tentangApa}</b> yang membuat kami tertawa. Siang bersama dia selalu terasa hidup, penuh keceriaan meski sederhana. Kadang kami hanya duduk berdua <b>${dimana}</b> sambil menikmati es teh dan makanan ringan, tapi rasanya sudah cukup membuat siang hari terasa berwarna.
+    </p>
+  `;
+}
